e2b203c5fee9ce86d4d557bf1c5840234e4a1200
JosephYusufov <jyusufov00@stuy.edu>
2019-03-15 14:06:43 -0400

    answered Q0 and Q1 using pseudocode

---
+++ 03-15-2019/Answers.java
@@ -0,0 +1,27 @@
+public class Answers {
+    public static void main(String[] args){
+        /*
+        Question 0:
+
+        A list of elements of a given type can only be represented by an array that is
+        of the same type. In the example below, that type is int.
+        */
+        int[] elements = new int[10];
+        System.out.println("==========");
+        for (int index :elements ) {
+            System.out.println( index );
+        }
+
+        /*
+        Question 1:
+
+        Create individual array fields for each type you want to represent.
+        Create another array field to keep track of the type of each element in the list.
+        Create a int field to count the number of elements in the list.
+        Create methods that fill up the type arrays which do not represent the next element. (Fill these spaces with any values, the program will consider them junk)
+        Incriment/decriment the counter after every addition/removal of an element.
+        */
+
+
+    }
+}
